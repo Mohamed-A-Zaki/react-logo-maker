2dbc46d6897cbb1258722c2bd21d08461b1c26a5
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-05-10 14:48:00 +0300

    Add react-best-gradient-color-picker package
    The commit introduces a new color picker component from the
    react-best-gradient-color-picker package to the IconColorPicker component.
    It removes the previous ColorPicker import from @mantine/core and its
    associated properties, replacing them with the new ColorPicker. This change
    simplifies the component by hiding unnecessary controls and color type
    buttons, focusing on a cleaner user interface. The package.json file has
    been updated to include the new dependency.

--- src/components/IconColorPicker.tsx
+++ src/components/IconColorPicker.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { ColorPicker, Text } from "@mantine/core";
+import { Text } from "@mantine/core";
+import ColorPicker from "react-best-gradient-color-picker";
 
 export default function IconColorPicker() {
   const [color, setColor] = useState("#f15757");
@@ -8,27 +9,10 @@ export default function IconColorPicker() {
     <div>
       <Text size="lg">Icon Color</Text>
       <ColorPicker
-        size="md"
-        format="hex"
         value={color}
         onChange={setColor}
-        swatchesPerRow={9}
-        swatches={[
-          "#2e2e2e",
-          "#868e96",
-          "#fa5252",
-          "#e64980",
-          "#be4bdb",
-          "#7950f2",
-          "#4c6ef5",
-          "#228be6",
-          "#15aabf",
-          "#12b886",
-          "#40c057",
-          "#82c91e",
-          "#fab005",
-          "#fd7e14",
-        ]}
+        hideColorTypeBtns
+        hideControls
       />
     </div>
   );
